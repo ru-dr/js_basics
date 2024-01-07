e0ed798d2ad8b38e2924a851c534aabf9c021975
Lazycoder26 <Rudra26_2004@outlook.com>
2024-01-07 12:31:16 +0530

    Update .gitignore and fix countDups function

--- sandbox/Play.js
+++ sandbox/Play.js
@@ -2,3 +2,19 @@
 // Open the console to see the output of your code.
 
 // Path: sandbox/Play.js
+
+// req vars ( currently ) : val, index , count ( maybe ), storage array
+
+function countDups(arr) {
+  let storageArr = new Array(arr.length).fill(0);
+  let finder = 1;
+  for (let index = 0; index < arr.length; index++) {
+    let currVal = arr[index];
+    storageArr[currVal]++;
+  }
+  if(storageArr.includes()){
+
+  }
+}
+
+console.log(countDups([1, 2, 2, 2]));
